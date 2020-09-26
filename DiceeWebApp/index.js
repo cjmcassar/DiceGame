f9e90bc8df513bb9c191ab0258884d15bf30fe8c
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-09-26 22:42:39 +0100

    - Added dice to website - Randomised the first dice

--- DiceeWebApp/index.js
+++ DiceeWebApp/index.js
@@ -1,1 +1,17 @@
-alert("Welcome!");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // Generate random number from 1 to 6
+
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // Randomise the dice images
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png through to images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//var image2 = document.querySelectorAll("img")[1];
+
+//var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+//image2.setAttribute("src", randomImageSource);
