5f9e5ec00b0064a62b71135f19dab27f1e4b7009
Christopher-John Cassar <cjmcassar@users.noreply.github.com>
2020-09-26 22:56:21 +0100

    - Added player notifications

--- DiceeWebApp/index.js
+++ DiceeWebApp/index.js
@@ -10,8 +10,24 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // Generate random number from 1 to 6
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //images/dice1.png through to images/dice6.png
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); // select the second image class and set the image source
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 Wins!"
+}
+
+// If player 2 wins
+else if (randomNumber2 > randomNumber1)
+{
+  document.querySelector("h1").innerHTML = "Player 2 Wins!"
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
